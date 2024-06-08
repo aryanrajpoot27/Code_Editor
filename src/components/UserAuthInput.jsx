156c66a27d012e5a96202924c2dbe07643183d95
aryanrajpoot27 <araryan1212@gmail.com>
2024-06-08 09:39:03 +0530

    password hide unhide functionality is added

--- src/components/UserAuthInput.jsx
+++ src/components/UserAuthInput.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { FaEnvelope, FaEye } from "react-icons/fa";
+import { FaEnvelope, FaEye, FaEyeSlash } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 const UserAuthInput = ({
   label,
@@ -8,22 +9,31 @@ const UserAuthInput = ({
   setStateFunction,
   Icon,
 }) => {
-  const [value, setvalue] = useState("");
+  const [value, setValue] = useState("");
+  const [showPass, setShowPass] = useState(true);
+  const handleTextChange = (e) => {
+    setValue(e.target.value);
+    setStateFunction(e.target.value);
+  };
   return (
     <div className="flex flex-col items-start justify-start gap-1">
       <label className="text-sm text-gray-300">{label}</label>
       <div className="flex items-center justify-center gap-3 w-full md:w-96 rounded-md px-4 py-1 bg-gray-200">
         <Icon className="text-text555 text-2xl" />
         <input
-          type={isPass ? "password" : "text"}
+          type={isPass && showPass ? "password" : "text"}
           placeholder={placeHolder}
           className="w-full flex-1 outline-none bg-transparent text-text555 text-lg  "
           value={value}
-          onChange={(e) => setvalue(e.target.value)}
+          onChange={handleTextChange}
         />
-        <div className="cursor-pointer ">
-          <FaEye className="text-text555 text-2xl " />
-        </div>
+        <motion.div onClick={() => setShowPass(!showPass)} whileTap={{ scale: 0.9 }} className="cursor-pointer ">
+          {showPass ? (
+            <FaEye className="text-text555 text-2xl " />
+          ) : (
+            <FaEyeSlash className="text-text555 text-2xl " />
+          )}
+        </motion.div>
       </div>
     </div>
   );
